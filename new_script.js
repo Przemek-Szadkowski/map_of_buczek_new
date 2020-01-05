197fbf20d361153a33a87859e0838e2c6f3e8189
Przemek-Szadkowski <przemoszadkowski@o2.pl>
2020-01-05 18:36:38 +0100

    add directions and new colors

--- new_script.js
+++ new_script.js
@@ -4,15 +4,17 @@ const options = select.querySelectorAll('option');
 const select_buildings = document.querySelector('.select_buildings');
 const building_options = select_buildings.querySelectorAll('option');
 
+const directions = document.querySelectorAll('div.directions i');
+const descriptions = document.querySelectorAll('div.directions p');
+
+
 const streets = document.querySelectorAll(".street");
-const buildings = document.querySelectorAll("div.map i.fas");
+const buildings = document.querySelectorAll("div.map i.build");
 const mapaSzadek = document.querySelector(".mapnorth");
 const streetsSzadek = document.querySelector(".mapnorth_top");
 
 
 function handleSelectStreets() {
-//    const select = document.querySelector('.select_streets');
-//    const options = select.querySelectorAll('option');
    options.forEach(option => {
        if(option.selected) {
            streets.forEach(street => {
@@ -21,10 +23,12 @@ function handleSelectStreets() {
                     if(street.classList.contains(`${option.value}`)) {
                         mapaSzadek.classList.remove('center');
                         streetsSzadek.classList.remove('center');
-                        // buildings.forEach(building => {
-                        //     building.classList.remove('visible2');
-                        //     building.style.opacity = '0';
-                        // })
+                        directions.forEach(direction => {
+                            direction.style.opacity = '1';
+                        })
+                        descriptions.forEach(description => {
+                            description.style.opacity = '1';
+                        })
                         street.classList.add('visible');
                             if(option.value === "szadek" || option.value === "wygwizdow") {
                             street.classList.add('visibleSz');
@@ -34,7 +38,13 @@ function handleSelectStreets() {
                                 building.classList.remove('visible2');
                                 building.style.opacity = '0';
                             })
-                            // "wyzerowanie" budynków przy włączeniu map dodatkowych
+                            directions.forEach(direction => {
+                                direction.style.opacity = '0';
+                            })
+                            descriptions.forEach(description => {
+                                description.style.opacity = '0';
+                            })
+                            // "wyzerowanie" selectu budynków przy włączeniu map dodatkowych
                             building_options.forEach(option => {
                                 option.selected = false;
                                 if(option.value === "---") {
@@ -49,8 +59,6 @@ function handleSelectStreets() {
 }
 
 
-// naniesienie kierunków n a mapę główną
-
 function handleSelectBuildings() {
     building_options.forEach(option => {
         if(option.selected) {
@@ -62,8 +70,22 @@ function handleSelectBuildings() {
                         street.classList.remove('visibleSz');
                     }
                 })
+                if(mapaSzadek.classList.contains('center')) {
+                    options.forEach(option => {
+                        option.selected = false;
+                        if(option.value === "---") {
+                            option.selected = true;
+                        }
+                    })
+                }
                 mapaSzadek.classList.remove('center');
                 streetsSzadek.classList.remove('center');
+                directions.forEach(direction => {
+                    direction.style.opacity = '1';
+                })
+                descriptions.forEach(description => {
+                    description.style.opacity = '1';
+                })
                 if(building.classList.contains(`${option.value}`)) {
                     building.classList.add('visible2');
                     building.addEventListener('animationend', function() {
