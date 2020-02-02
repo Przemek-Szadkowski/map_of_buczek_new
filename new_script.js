8c4a81cead3003c0fa877d1e00e5da951008c02d
Przemek-Szadkowski <przemoszadkowski@o2.pl>
2020-02-02 20:00:54 +0100

    New version :)))

--- new_script.js
+++ new_script.js
@@ -4,6 +4,11 @@ const options = select.querySelectorAll('option');
 const select_buildings = document.querySelector('.select_buildings');
 const building_options = select_buildings.querySelectorAll('option');
 
+const aside = document.querySelector('aside');
+const select_buildings_aside = document.querySelector('.select_buildings_aside');
+const building_options_aside = select_buildings_aside.querySelectorAll('option');
+
+
 const directions = document.querySelectorAll('div.directions i');
 const descriptions = document.querySelectorAll('div.directions p');
 
@@ -12,6 +17,8 @@ const streets = document.querySelectorAll(".street");
 const buildings = document.querySelectorAll("div.map i.build");
 const mapaSzadek = document.querySelector(".mapnorth");
 const streetsSzadek = document.querySelector(".mapnorth_top");
+
+const buttons = document.querySelectorAll('div button');
 
 
 function handleSelectStreets() {
@@ -96,3 +103,84 @@ function handleSelectBuildings() {
          }
      })
  }
+
+ function handleSelectBuildingsAside() {
+    building_options_aside.forEach(option => {
+        if(option.selected) {
+            buildings.forEach(building => {
+                building.classList.remove('visible2');
+                building.style.opacity = '0';
+                streets.forEach(street => {
+                    if(street.classList.contains('visibleSz')) {
+                        street.classList.remove('visibleSz');
+                    }
+                })
+                if(mapaSzadek.classList.contains('center')) {
+                    options.forEach(option => {
+                        option.selected = false;
+                        if(option.value === "---") {
+                            option.selected = true;
+                        }
+                    })
+                }
+                mapaSzadek.classList.remove('center');
+                streetsSzadek.classList.remove('center');
+                directions.forEach(direction => {
+                    direction.style.opacity = '1';
+                })
+                descriptions.forEach(description => {
+                    description.style.opacity = '1';
+                })
+                if(building.classList.contains(`${option.value}`)) {
+                    building.classList.add('visible2');
+                    building.addEventListener('animationend', function() {
+                        building.style.opacity = '1';
+                    })
+                }
+            })
+         }
+     })
+ }
+
+ function handleButtonClick() {
+     streets.forEach(street => {
+        street.classList.remove('visible');
+        street.classList.remove('visibleSz');
+        mapaSzadek.classList.remove('center');
+        streetsSzadek.classList.remove('center');
+        directions.forEach(direction => {
+            direction.style.opacity = '1';
+        })
+        descriptions.forEach(description => {
+            description.style.opacity = '1';
+        })
+     })
+     if(this.textContent === "Szadek" || this.textContent === "Wygwizdów") {
+         console.log("NOW!")
+        mapaSzadek.classList.add('center');
+        streetsSzadek.classList.add('center');
+        buildings.forEach(building => {
+            building.classList.remove('visible2');
+            building.style.opacity = '0';
+        })
+        directions.forEach(direction => {
+            direction.style.opacity = '0';
+        })
+        descriptions.forEach(description => {
+            description.style.opacity = '0';
+        })
+        if(this.textContent === "Szadek") {
+            document.querySelector('.szadek').classList.toggle("visibleSz");
+            } else if (this.textContent === "Wygwizdów") {
+                document.querySelector('.wygwizdow').classList.toggle("visibleSz");
+            };
+    } else {
+    document.querySelector('[data-ulica="' + this.className + '"]').classList.toggle("visible");
+        }
+    };
+
+
+
+ buttons.forEach(button => {
+     button.addEventListener('click', handleButtonClick);
+ });
